261dbfcc09a86eb4f6982b30e68697b309751390
anjolly <jollymanster@gmail.com>
2017-07-21 19:07:24 -0400

    Updated API to use token authentication; added tokenizer project to grab google play authentication token

--- gplaycrawler/gplayapi/config.py
+++ gplaycrawler/gplayapi/config.py
@@ -19,23 +19,25 @@ if not os.path.isfile('auth.json'):
     with open('auth.json', 'wb') as f:
         LANG            = raw_input_with_default('LANG', 'en_US')
         ANDROID_ID      = raw_input_with_default('ANDROID_ID', 'xxxxxxxxxxxxxxxx')
-        GOOGLE_LOGIN    = raw_input_with_default('GOOGLE_LOGIN', 'account@gmail.com')
-        GOOGLE_PASSWORD = raw_input_with_default('GOOGLE_PASSWORD', '***')
+        #GOOGLE_LOGIN    = raw_input_with_default('GOOGLE_LOGIN', 'account@gmail.com')
+        #GOOGLE_PASSWORD = raw_input_with_default('GOOGLE_PASSWORD', '***')
+        AUTH_TOKEN      = raw_input_with_default('AUTH_TOKEN', '***')
         f.write(json.dumps(
         {
             'LANG':LANG,
             'ANDROID_ID':ANDROID_ID,
-            'GOOGLE_LOGIN':GOOGLE_LOGIN,
-            'GOOGLE_PASSWORD':GOOGLE_PASSWORD
+            #'GOOGLE_LOGIN':GOOGLE_LOGIN,
+            #'GOOGLE_PASSWORD':GOOGLE_PASSWORD
+			'AUTH_TOKEN':AUTH_TOKEN
         }
         ))
 elif os.path.isfile('auth.json'):
     with open('auth.json', 'rb') as f:config = json.load(f)
     LANG            = config['LANG']
     ANDROID_ID      = config['ANDROID_ID']
-    GOOGLE_LOGIN    = config['GOOGLE_LOGIN']
-    GOOGLE_PASSWORD = config['GOOGLE_PASSWORD']
-    AUTH_TOKEN      = None
+    GOOGLE_LOGIN    = None #config['GOOGLE_LOGIN']
+    GOOGLE_PASSWORD = None #config['GOOGLE_PASSWORD']
+    AUTH_TOKEN      = config['AUTH_TOKEN']
 
-if any([each == None for each in [ANDROID_ID, GOOGLE_LOGIN, GOOGLE_PASSWORD]]):
+if any([each == None for each in [ANDROID_ID, AUTH_TOKEN]]):
     raise Exception("Config not set. Delete auth.json and run config.py again.")
